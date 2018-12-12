7dd9c265ace733ea7302ba511942dd98b1fed87c
Jose Lopez <joselopezg8@gmail.com>
2018-12-11 22:07:57 -0400

    [REM] removed unnused code

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "l10n_do_currency_update",
+    'name': "Dominican Banks Currency Update",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
+        Updates company secondary currency rates from dominican banks""",
 
     'author': "Indexa",
     'website': "https://www.indexa.do",
@@ -16,8 +11,8 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Accounting',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
