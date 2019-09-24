ac047d0617308765c44ac35e11bbfdaf0581820d
José López <joselopezg8@gmail.com>
2019-09-23 22:08:08 -0400

    Add files via upload

---
+++ l10n_do_currency_update/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Dominican Banks Currency Update",
+
+    'summary': """
+        Updates company secondary currency rates from dominican banks""",
+
+    'author': "Indexa",
+    'website': "https://www.indexa.do",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',
+                'account'],
+
+    # always loaded
+    'data': [
+        'data/ir_cron_data.xml',
+        'data/ir_config_parameter_data.xml',
+        'views/account_views.xml',
+        'views/account_config_settings_views.xml',
+        'wizard/res_company_currency_hash_wizard_views.xml',
+    ],
+    'demo': [
+        'demo/res_company_demo.xml',
+    ],
+}
