99363a571191f4f90b736f14d38d7e5708c4456d
Gustavo Valverde <g.valverde02@gmail.com>
2020-01-06 23:17:07 -0400

    [REF] external_services_addons: Include stdnum as fallback
    This also refactors a few methods to consider a bunch of corner-cases and a better UX

--- l10n_do_rnc_validation/models/res_company.py
+++ l10n_do_rnc_validation/models/res_company.py
@@ -1,5 +1,5 @@
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ResCompany(models.Model):
@@ -8,3 +8,21 @@ class ResCompany(models.Model):
     can_validate_rnc = fields.Boolean(
         default=True,
     )
+
+    @api.onchange("name")
+    def _onchange_company_name(self):
+        if self.name:
+            result = self.env['res.partner'].validate_rnc_cedula(
+                self.name, model='res.company')
+            if result:
+                self.name = result.get('name')
+                self.vat = result.get('vat')
+
+    @api.onchange("vat")
+    def _onchange_company_vat(self):
+        if self.vat:
+            result = self.env['res.partner'].validate_rnc_cedula(
+                self.vat, model='res.company')
+            if result:
+                self.name = result.get('name')
+                self.vat = result.get('vat')
