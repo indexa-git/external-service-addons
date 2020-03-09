eb48f1e645ff3151786aad0096644f275b8ad380
Jose Lopez <joselopezg8@gmail.com>
2020-03-09 15:16:31 -0400

    [REF] l10n_do_currency_update: system param token

--- l10n_do_currency_update/__manifest__.py
+++ l10n_do_currency_update/__manifest__.py
@@ -14,7 +14,6 @@
         'data/ir_config_parameter_data.xml',
         'views/res_config_settings_views.xml',
         'views/account_views.xml',
-        'wizard/res_company_currency_hash_wizard_views.xml',
     ],
     'demo': [
         'demo/res_company_demo.xml',
