ae6c70ec470de7a52fe92a25347f72b2a096815d
José López <joselopezg8@gmail.com>
2020-05-18 00:18:53 -0400

    [12.0][REF] l10n_do_rnc_validation: fixes & improvements (#21) (#22) (#23) (#24)
    * [12.0][REF] l10n_do_rnc_validation: fixes & improvements (#21)
    
    * [REF] cloudbuild: update
    
    * [REF] cloudbuild: update
    
    * [IMP] l10n_do_rnc_validation: refact code so only one request is made when create/write
    
    * [REF] l10n_do_rnc_validation: clean code a bit
    
    * [REF] l10n_do_rnc_validation: use context instead of function param
    
    * [REF] l10n_do_rnc_validation: remove TODO comment
    
    * [FIX] l10n_do_rnc_validation: IndexError when removing vat
    
    * [REF] l10n_do_rnc_validation: fixes & improvements
    
    * [FIX] l10n_do_rnc_validation: Expected singleton
    
    * [FIX] l10n_do_rnc_validation: Expected singleton

--- l10n_do_rnc_validation/models/res_company.py
+++ l10n_do_rnc_validation/models/res_company.py
@@ -1,27 +1,10 @@
-from odoo import fields, models, api
+
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    can_validate_rnc = fields.Boolean(default=True,)
-
-    @api.onchange("name")
-    def _onchange_company_name(self):
-        if self.name:
-            result = self.env['res.partner'].validate_rnc_cedula(
-                self.name, model='res.company'
-            )
-            if result:
-                self.name = result.get('name')
-                self.vat = result.get('vat')
-
-    @api.onchange("vat")
-    def _onchange_company_vat(self):
-        if self.vat:
-            result = self.env['res.partner'].validate_rnc_cedula(
-                self.vat, model='res.company'
-            )
-            if result:
-                self.name = result.get('name')
-                self.vat = result.get('vat')
+    can_validate_rnc = fields.Boolean(
+        default=True,
+    )
