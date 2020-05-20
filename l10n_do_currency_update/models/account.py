b28beb9228ed9f4d0212360913aec2743e53b95d
José López <joselopezg8@gmail.com>
2020-05-19 21:04:29 -0400

    [13.0][REF] l10n_do_rnc_validation: fixes & improvements (#28)
    * [12.0][REF] l10n_do_rnc_validation: fixes & improvements (#21) (#22)
    
    * [12.0][REF] l10n_do_rnc_validation: fixes & improvements (#21)
    
    * [REF] cloudbuild: update
    
    * [REF] cloudbuild: update
    
    * [IMP] l10n_do_rnc_validation: refact code so only one request is made when create/write
    
    * [REF] l10n_do_rnc_validation: clean code a bit
    
    * [REF] l10n_do_rnc_validation: use context instead of function param
    
    * [REF] l10n_do_rnc_validation: remove TODO comment
    
    * [FIX] l10n_do_rnc_validation: IndexError when removing vat
    
    * [REF] l10n_do_rnc_validation: fixes & improvements
    
    * [FIX] l10n_do_rnc_validation: Expected singleton
    
    * [FIX] l10n_do_rnc_validation: Expected singleton
    
    * [12.0][REF] l10n_do_currency_update: remove invoice rate feature ( (#27)
    
    * [REF] cloudbuild: update
    
    * [12.0][REF] l10n_do_rnc_validation: fixes & improvements (#21)
    
    * [REF] cloudbuild: update
    
    * [REF] cloudbuild: update
    
    * [IMP] l10n_do_rnc_validation: refact code so only one request is made when create/write
    
    * [REF] l10n_do_rnc_validation: clean code a bit
    
    * [REF] l10n_do_rnc_validation: use context instead of function param
    
    * [REF] l10n_do_rnc_validation: remove TODO comment
    
    * [FIX] l10n_do_rnc_validation: IndexError when removing vat
    
    * [REF] l10n_do_rnc_validation: fixes & improvements
    
    * [FIX] l10n_do_rnc_validation: Expected singleton
    
    * [FIX] l10n_do_rnc_validation: Expected singleton
    
    * [REF] l10n_do_currency_update: remove invoice rate feature (#25) (#26)
    
    * [REF] l10n_do_currency_update: remove invoice rate feature (#25)
    
    * [REF] l10n_do_currency_update: update translation
    
    * [REF] cloudbuild: update

--- l10n_do_currency_update/models/account.py
+++ l10n_do_currency_update/models/account.py
@@ -1,58 +1,13 @@
 #  Copyright (c) 2018 - Indexa SRL. (https://www.indexa.do) <info@indexa.do>
 #  See LICENSE file for full licensing details.
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     rate = fields.Float(
-        compute='_compute_rate',
-        store=True,
+        # Deprecated.
+        # Do not forwardport to v14.
     )
-    show_rate = fields.Boolean(
-        compute='_compute_show_rate'
-    )
-
-    @api.depends('currency_id', 'company_id')
-    def _compute_show_rate(self):
-        for inv in self:
-            inv.show_rate = not inv.company_id.currency_id == inv.currency_id
-
-    def get_invoice_rate(self, date):
-
-        Rate = self.env['res.currency.rate']
-
-        rate_id = Rate.search([('name', '=', date),
-                               ('currency_id', '=', self.currency_id.id),
-                               ('company_id', '=', self.company_id.id)])
-
-        if rate_id:
-            return 1 / rate_id.rate
-
-        before_rate_id = Rate.search(
-            [('name', '<', date), ('currency_id', '=', self.currency_id.id),
-             ('company_id', '=', self.company_id.id)], order='name desc', limit=1)
-
-        return 1 / before_rate_id.rate if before_rate_id else 1
-
-    @api.depends('state', 'invoice_date', 'currency_id')
-    def _compute_rate(self):
-        for inv in self.filtered(lambda i: i.invoice_date):
-            if not inv.rate:
-                inv.rate = inv.get_invoice_rate(inv.invoice_date)
-            else:
-                inv.rate = inv.rate
-
-    def action_show_currency(self):
-        self.ensure_one()
-        view_id = self.env.ref('base.view_currency_form')
-        return {
-            'type': 'ir.actions.act_window',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'res.currency',
-            'view_id': view_id.id,
-            'res_id': self.currency_id.id
-        }
