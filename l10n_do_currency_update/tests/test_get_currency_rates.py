783d863617464d19fc803f5acf726975647200a0
Jose Lopez <joselopezg8@gmail.com>
2020-06-29 20:07:51 -0400

    [IMP] l10n_do_currency_update: added basic unitest

---
+++ l10n_do_currency_update/tests/test_get_currency_rates.py
@@ -0,0 +1,18 @@
+
+from odoo import fields
+from odoo.tests.common import TransactionCase
+
+
+class GetCurrencyRatesTest(TransactionCase):
+
+    def test_001_get_currency_rates(self):
+
+        data = self.env["res.company"].get_currency_rates(
+            {"bank": "bpd", "date": fields.Date.today()},
+            "a79c2dfc-858d-4813-bb77-7695c1c320db"
+        )
+
+        status = data.get("status", False)
+
+        assert status
+        assert status == "success"
