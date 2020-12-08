ae74610beca83bf418f75c27f81ece50935877be
Jose Lopez <joselopezg8@gmail.com>
2020-12-07 23:50:16 -0400

    [ADD] start working with l10n_do_ncf_validation

---
+++ l10n_do_ncf_validation/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Dominican NCF Validation",
+    "version": "13.0.0.0.0",
+    "summary": "Validate NCF from external service",
+    "category": "Extra Tools",
+    "author": "Indexa",
+    "website": "https://www.indexa.do",
+    "depends": ["l10n_do_accounting"],
+    "data": ["views/res_config_settings_views.xml"],
+}
