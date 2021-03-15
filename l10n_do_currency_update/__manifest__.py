1c85c5b2640fd1bfac458c79ed13859846f474a7
FernandoRFR02 <fernandorfr02@gmail.com>
2021-03-15 16:15:21 -0400

    [REF] l10n_do_currency_update : black

--- l10n_do_currency_update/__manifest__.py
+++ l10n_do_currency_update/__manifest__.py
@@ -1,21 +1,20 @@
 {
-    'name': "Dominican Banks Currency Update",
-    'summary': """
+    "name": "Dominican Banks Currency Update",
+    "summary": """
     Updates company secondary currency rates from dominican banks
     """,
-
-    'author': "Indexa",
-    'website': "https://www.indexa.do",
-    'category': 'Accounting',
-    'version': "14.0.1.0.0",
-    'depends': ['account'],
-    'data': [
-        'data/ir_cron_data.xml',
-        'data/ir_config_parameter_data.xml',
-        'views/res_config_settings_views.xml',
+    "author": "Indexa",
+    "website": "https://www.indexa.do",
+    "category": "Accounting",
+    "version": "14.0.1.0.0",
+    "depends": ["account"],
+    "data": [
+        "data/ir_cron_data.xml",
+        "data/ir_config_parameter_data.xml",
+        "views/res_config_settings_views.xml",
     ],
-    'demo': [
-        'demo/res_company_demo.xml',
+    "demo": [
+        "demo/res_company_demo.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- l10n_do_currency_update/tests/test_get_currency_rates.py
+++ l10n_do_currency_update/tests/test_get_currency_rates.py
@@ -5,14 +5,14 @@ from odoo.tests import tagged
 
 @tagged("post_install", "-at_install")
 class GetCurrencyRatesTest(AccountTestInvoicingCommon):
-
     def test_001_get_currency_rates(self):
 
         data = self.env["res.company"].get_currency_rates(
             {"bank": "bpd", "date": fields.Date.today()},
-            "a79c2dfc-858d-4813-bb77-7695c1c320db"
+            "a79c2dfc-858d-4813-bb77-7695c1c320db",
         )
         import ast
+
         data = ast.literal_eval(data)
         status = data.get("status", False)
 
