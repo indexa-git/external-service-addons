96fcdf904447c94baff2ad7ab2590516ffcd0aa8
Jose Lopez <joselopezg8@gmail.com>
2021-08-06 14:02:01 -0400

    [REF] l10n_do_ecf_invoicing: get ncf exp date from invoice

--- l10n_do_ecf_invoicing/models/account_move.py
+++ l10n_do_ecf_invoicing/models/account_move.py
@@ -232,7 +232,7 @@ class AccountMove(models.Model):
             {
                 "TipoeCF": self.get_l10n_do_ncf_type(),
                 "eNCF": self.ref,
-                "FechaVencimientoSecuencia": "31-12-2021",  # TODO: get this from ncf_expiration_date
+                "FechaVencimientoSecuencia": self.ncf_expiration_date,
             }
         )
 
