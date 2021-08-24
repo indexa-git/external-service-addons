5c65148dc649685120c4336c41a2f67b7df5a679
Jose Lopez <joselopezg8@gmail.com>
2021-08-24 11:37:48 -0400

    [REF] l10n_do_ecf_invoicing: update manifest

--- l10n_do_ecf_invoicing/__manifest__.py
+++ l10n_do_ecf_invoicing/__manifest__.py
@@ -1,22 +1,16 @@
 {
-    "name": "l10n_do_ecf_invoicing",
+    "name": "Dominican Republic EDI Invoicing",
     "summary": """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-    "description": """
-        Long description of module's purpose
-    """,
+        Dominican Republic DGII Electronic Invoicing""",
     "author": "Indexa",
     "website": "https://www.indexa.do",
     "category": "Accounting",
     "version": "13.0.1.0.0",
     "depends": ["l10n_do_accounting", "l10n_do_debit_note"],
     "data": [
-        # 'security/ir.model.access.csv',
         "views/account_views.xml",
         "views/res_config_settings_views.xml",
         "data/ir_config_parameter_data.xml",
         "data/ir_cron_data.xml",
     ],
-    "demo": ["demo/demo.xml"],
 }
