dc59590e7242953522b6e26dc621a0dde1674e64
Fernando R. Figuereo Roa <48818804+FernandoRFR02@users.noreply.github.com>
2021-08-25 11:52:13 -0400

    [MIG] l10n_do_rnc_validation : Migration to v14 (#64)

--- l10n_do_ncf_validation/__manifest__.py
+++ l10n_do_ncf_validation/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Dominican NCF Validation",
-    "version": "13.0.0.0.0",
+    "version": "14.0.1.0.0",
     "summary": "Validate NCF from external service",
     "category": "Extra Tools",
     "author": "Indexa",
@@ -10,5 +10,5 @@
         "data/ir_config_parameter_data.xml",
         "views/res_config_settings_views.xml",
     ],
-    "installable": False,
+    "installable": True,
 }
