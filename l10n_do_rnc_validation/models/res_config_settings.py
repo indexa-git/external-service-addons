58381567b75ae8b51b31df764e0ca889c668756e
Fernando R. Figuereo Roa <48818804+FernandoRFR02@users.noreply.github.com>
2021-08-25 11:52:13 -0400

    [MIG] l10n_do_rnc_validation : Migration to v14 (#64)

--- l10n_do_rnc_validation/models/res_config_settings.py
+++ l10n_do_rnc_validation/models/res_config_settings.py
@@ -2,8 +2,9 @@ from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     can_validate_rnc = fields.Boolean(
-        related="company_id.can_validate_rnc", readonly=False,
+        related="company_id.can_validate_rnc",
+        readonly=False,
     )
