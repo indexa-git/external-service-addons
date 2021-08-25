f96718ee2150315656cf00a002b9591315630667
Fernando R. Figuereo Roa <48818804+FernandoRFR02@users.noreply.github.com>
2021-08-25 11:52:13 -0400

    [MIG] l10n_do_rnc_validation : Migration to v14 (#64)

--- l10n_do_currency_update/tests/test_get_currency_rates.py
+++ l10n_do_currency_update/tests/test_get_currency_rates.py
@@ -1,17 +1,18 @@
-
 from odoo import fields
-from odoo.tests.common import TransactionCase
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
+from odoo.tests import tagged
 
 
-class GetCurrencyRatesTest(TransactionCase):
-
+@tagged("post_install", "-at_install")
+class GetCurrencyRatesTest(AccountTestInvoicingCommon):
     def test_001_get_currency_rates(self):
 
         data = self.env["res.company"].get_currency_rates(
             {"bank": "bpd", "date": fields.Date.today()},
-            "a79c2dfc-858d-4813-bb77-7695c1c320db"
+            "a79c2dfc-858d-4813-bb77-7695c1c320db",
         )
         import ast
+
         data = ast.literal_eval(data)
         status = data.get("status", False)
 
--- l10n_do_rnc_validation/__manifest__.py
+++ l10n_do_rnc_validation/__manifest__.py
@@ -1,21 +1,19 @@
 {
-    'name': 'Dominican Tax ID Validation',
-    'version': '13.0.0.0.0',
-    'summary': 'Validate RNC/Cédula from external service',
-    'category': 'Extra Tools',
-    'author': 'Guavana,'
-              'Indexa,'
-              'Iterativo',
-    'website': 'https://github.com/odoo-dominicana',
-    'license': 'LGPL-3',
-    'depends': [
-        'base',
-        'base_setup',
+    "name": "Dominican Tax ID Validation",
+    "version": "14.0.1.0.0",
+    "summary": "Validate RNC/Cédula from external service",
+    "category": "Extra Tools",
+    "author": "Guavana," "Indexa," "Iterativo",
+    "website": "https://github.com/odoo-dominicana",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+        "base_setup",
     ],
-    'data': [
-        'views/res_partner_views.xml',
-        'views/res_config_settings_views.xml',
-        'data/ir_config_parameter_data.xml',
+    "data": [
+        "views/res_partner_views.xml",
+        "views/res_config_settings_views.xml",
+        "data/ir_config_parameter_data.xml",
     ],
-    "installable": False,
+    "installable": True,
 }
