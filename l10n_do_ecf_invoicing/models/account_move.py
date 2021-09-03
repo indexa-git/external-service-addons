cc4ec5c9bcf0a777c03d023e20040c178a25e37d
Jose Lopez <joselopezg8@gmail.com>
2021-09-03 11:59:42 -0400

    [FIX] l10n_do_ecf_invoicing: prevent TypeError if missing any of those two dates

--- l10n_do_ecf_invoicing/models/account_move.py
+++ l10n_do_ecf_invoicing/models/account_move.py
@@ -125,7 +125,9 @@ class AccountMove(models.Model):
         self.ensure_one()
         # TODO: evaluate payment type 3 <Gratuito> Check DGII docs
         if not self.invoice_payment_term_id and self.invoice_date_due:
-            if self.invoice_date_due > self.invoice_date:
+            if (
+                self.invoice_date_due and self.invoice_date
+            ) and self.invoice_date_due > self.invoice_date:
                 return 2
             else:
                 return 1
