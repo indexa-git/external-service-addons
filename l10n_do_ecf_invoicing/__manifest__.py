b0ff79ed69bd3b58efe80977143ceaaae3188bf0
José López <joselopezg8@gmail.com>
2021-10-29 17:34:30 -0400

    [FIX] l10n_do_ecf_invoicing: always send Pago al Exterior Electrónica's Retencion data (#101)
    Co-authored-by: AndrésR <androdriguez14@gmail.com>

--- l10n_do_ecf_invoicing/__manifest__.py
+++ l10n_do_ecf_invoicing/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "Indexa",
     "website": "https://www.indexa.do",
     "category": "Accounting",
-    "version": "13.0.1.2.2",
+    "version": "13.0.1.2.3",
     "depends": ["l10n_do_accounting", "l10n_do_debit_note"],
     "data": [
         "views/account_views.xml",
