17d2e8bbe43f9fb6e064e7a5bb2d27a4d0e158db
José López <joselopezg8@gmail.com>
2021-11-09 20:16:35 -0400

    [IMP] l10n_do_ecf_invoicing: implemented function to create missing ecf sequences (#106)

--- l10n_do_ecf_invoicing/__manifest__.py
+++ l10n_do_ecf_invoicing/__manifest__.py
@@ -5,12 +5,13 @@
     "author": "Indexa",
     "website": "https://www.indexa.do",
     "category": "Accounting",
-    "version": "13.0.1.3.3",
+    "version": "13.0.1.4.3",
     "depends": ["l10n_do_accounting", "l10n_do_debit_note"],
     "data": [
         "views/account_views.xml",
         "views/res_config_settings_views.xml",
         "data/ir_config_parameter_data.xml",
         "data/ir_cron_data.xml",
+        "data/account_journal_data.xml",
     ],
 }
