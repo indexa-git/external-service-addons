ef4b36a5d3e26c1013607b15f7901f12a42d4960
Jose Lopez <joselopezg8@gmail.com>
2021-11-11 21:27:48 -0400

    [IMP] l10n_do_ecf_invoicing: restrict ecf sending environment

--- l10n_do_ecf_invoicing/models/res_company.py
+++ l10n_do_ecf_invoicing/models/res_company.py
@@ -1,4 +1,6 @@
-from odoo import models, fields
+from werkzeug import urls
+from odoo import models, fields, api, _
+from odoo .exceptions import ValidationError
 
 
 class ResCompany(models.Model):
@@ -13,3 +15,32 @@ class ResCompany(models.Model):
     l10n_do_send_ecf_on_payment = fields.Boolean(
         "Send ECF On Payment",
     )
+
+    @api.constrains("l10n_do_ecf_service_env")
+    def _check_ecf_service_env(self):
+        """
+        Checks ECF environment consistency. Once an ECF is issued to one environment,
+        a distinct one can't be used.
+        """
+        for company in self:
+            signed_invoice = self.env["account.move"].search(
+                [
+                    ("l10n_do_electronic_stamp", "!=", False),
+                    ("company_id", "=", company.id),
+                ],
+                limit=1,
+            )
+            if not signed_invoice:
+                continue
+
+            company_service_env = company.l10n_do_ecf_service_env
+            current_service_env = urls.url_unquote(
+                signed_invoice.l10n_do_electronic_stamp
+            ).split("/")[3]
+            if company_service_env != current_service_env:
+                raise ValidationError(
+                    _(
+                        "You cannot change company ECF Environment since there "
+                        "are invoices sent to %s" % current_service_env
+                    )
+                )
