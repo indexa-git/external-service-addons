244b6bf448ce28d84895f982935c007a03bd9f1e
Jose Lopez <joselopezg8@gmail.com>
2022-03-31 18:01:09 -0400

    [FIX] l10n_do_ecf_invoicing: handle unexpected error from DGII

--- l10n_do_ecf_invoicing/__manifest__.py
+++ l10n_do_ecf_invoicing/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "Indexa",
     "website": "https://www.indexa.do",
     "category": "Accounting",
-    "version": "14.0.1.9.12",
+    "version": "14.0.1.10.12",
     "depends": ["l10n_do_accounting"],
     "data": [
         "views/account_views.xml",
