4ea34330f51911723681af58ead71382f2d6256b
Jose Lopez <joselopezg8@gmail.com>
2022-05-13 14:21:12 -0400

    [IMP] l10n_do_ecf_invoicing: do not set as SignedPending if error

--- l10n_do_ecf_invoicing/__manifest__.py
+++ l10n_do_ecf_invoicing/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "Indexa",
     "website": "https://www.indexa.do",
     "category": "Accounting",
-    "version": "14.0.1.10.17",
+    "version": "14.0.1.10.18",
     "depends": ["l10n_do_accounting"],
     "data": [
         "views/account_views.xml",
