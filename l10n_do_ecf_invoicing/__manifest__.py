b6cfdbda22d3e688934c3223852792ea8bc7cfd1
Jose Lopez <joselopezg8@gmail.com>
2022-05-23 15:53:47 -0400

    [IMP] l10n_do_ecf_invoicing: use of correct invoice main partner field

--- l10n_do_ecf_invoicing/__manifest__.py
+++ l10n_do_ecf_invoicing/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "Indexa",
     "website": "https://www.indexa.do",
     "category": "Accounting",
-    "version": "13.0.1.13.18",
+    "version": "13.0.1.13.19",
     "depends": ["l10n_do_accounting", "l10n_do_debit_note"],
     "data": [
         "views/account_views.xml",
