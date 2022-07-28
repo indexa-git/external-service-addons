3fa99b9ad60e5baea51d7fd16bc2035b2153748e
José López <joselopezg8@gmail.com>
2022-07-28 10:17:26 -0400

    [REF] l10n_do_ecf_invoicing: make _send_ecf_submit_request() easier to extend (#181)

--- l10n_do_ecf_invoicing/__manifest__.py
+++ l10n_do_ecf_invoicing/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "Indexa",
     "website": "https://www.indexa.do",
     "category": "Accounting",
-    "version": "13.0.1.19.19",
+    "version": "13.0.1.20.19",
     "depends": ["l10n_do_accounting", "l10n_do_debit_note"],
     "data": [
         "views/account_views.xml",
