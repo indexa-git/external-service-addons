a9e4ba85e2427d0199a6be559c661968052b4131
Jorge Conce <jconcehernandez@gmail.com>
2024-03-27 09:59:11 -0400

    [MIG] l10n_do_ncf_validation: Migracion v16

--- l10n_do_ncf_validation/__manifest__.py
+++ l10n_do_ncf_validation/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Dominican NCF Validation",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "summary": "Validate NCF from external service",
     "category": "Extra Tools",
     "license": "LGPL-3",
