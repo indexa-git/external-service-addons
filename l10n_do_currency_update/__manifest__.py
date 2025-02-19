7d8463d04b1e17b0596e417697606dd2ec1fba74
Fernando Figuereo <fernandorfr02@gmail.com>
2025-02-19 10:31:04 -0400

    [IMP] l10n_do_currency_update : add forcecreate false

--- l10n_do_currency_update/__manifest__.py
+++ l10n_do_currency_update/__manifest__.py
@@ -7,7 +7,11 @@
     "website": "https://www.indexa.do",
     "category": "Accounting",
     "license": "LGPL-3",
+<<<<<<< HEAD
     "version": "17.0.1.0.1",
+=======
+    "version": "15.0.1.0.2",
+>>>>>>> 19aee4e ([IMP] l10n_do_currency_update : add forceupdate false)
     "depends": ["account"],
     "data": [
         "data/ir_cron_data.xml",
